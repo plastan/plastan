dc21cf54f6b2abdde657e3c0c9e4a7bd461eae46
plastan <92996519+plastan@users.noreply.github.com>
2021-10-22 21:52:15 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import websockets
+import asyncio
+
+port = 7890
+ip = "103.161.55.48"
+print(f"server listening to {port}")
+
+
+file = open("store.txt", "w")
+
+
+async def echo(websocket, path):
+    print("A client just connected")
+    print(path)
+    try:
+        async for message in websocket:
+            print("recieved message from client" + message)
+            await websocket.send(message)
+            file.write(message + "\n\n")
+    except websockets.exceptions.ConnectionClosed as e:
+        print("a client just disconnected")
+        file.close()
+
+
+start_server = websockets.serve(echo, ip, port)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
